10bf9bdfc70954918d2cf635dccc49e01f5cdbf9
eyobScript <eyobbmulugeta@gmail.com>
2025-04-08 23:50:49 +0300

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <MovieCard movie={{title: "eyob king", release_date: "2025"}}/>
+      <Home />
     </>
   );
 }
