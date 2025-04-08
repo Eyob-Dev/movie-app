3e016a24857a75455fc1607ecc4ad3669e2866dc
eyobScript <eyobbmulugeta@gmail.com>
2025-04-08 22:32:09 +0300

    move card component

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,3 +1,22 @@
 function MovieCard({movie}) {
-    
+    const onFavoriteClick = () => {
+        alert('Clicked on favorite button');
+    }
+    return 
+    <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title} />
+            <div className="movie-overlay">
+                <button className="favorite-button" onClick={onFavoriteClick}>
+                    ♡
+                </button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
+    </div>
 }
+
+export default MovieCard
