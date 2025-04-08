ed22435edc902e96036fa5e175391dce3903c4cd
eyobScript <eyobbmulugeta@gmail.com>
2025-04-08 23:52:18 +0300

    state

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import MovieCard from "../components/MovieCard";
 import { useState } from "react";
 
 function Home() {
+    
   const [searchQuery, setSearchQuery] = useState("");
 
   const movies = [
