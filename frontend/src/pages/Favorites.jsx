41d8a0812a51b184e8b66584b2b372dcef204025
eyobScript <eyobbmulugeta@gmail.com>
2025-04-08 23:56:09 +0300

    h

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 function favorite(){
     return (
         <div className="favorite">
+            <h1>Favorites</h1>
+            <p>This is the favorites page.</p>
+        <div className="favorite">
             <h1>Favorites </h1>
             <p>This is the favorites page.</p>
         </div>
