3393938540b58ba4cd363fd43a5aeac541548cc0
eyobScript <eyobbmulugeta@gmail.com>
2025-04-09 00:01:00 +0300

    add movie to favorite

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -3,6 +3,7 @@ function favorite(){
     return (
         <div className="favorite-empty">
             <h2>No favorite movie yet.</h2>
+            <p>Click on the heart icon to add a movie to your favorites.</p>
         </div>
     );
 }
