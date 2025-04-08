aee17e12398d5f93fa6bf524688b987dc773b53d
eyobScript <eyobbmulugeta@gmail.com>
2025-04-08 23:59:50 +0300

    no movie

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 function favorite(){
     return (
         <div className="favorite-empty">
-            <h2>Favorites</h2>
+            <h2>No favorite movie yet.</h2>
         </div>
     );
 }
