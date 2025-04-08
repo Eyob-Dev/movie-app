d12ce736f17861d2f4fb816f0a06820b5df4ddcb
eyobScript <eyobbmulugeta@gmail.com>
2025-04-08 23:50:56 +0300

    home

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "john", release_date: "2023" },
+    { id: 2, title: "doe", release_date: "2024" },
+    { id: 3, title: "jane", release_date: "2025" },
+  ];
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    alert(searchQuery);
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          className="search-input"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movie-grid">
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery.toLowerCase()) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
