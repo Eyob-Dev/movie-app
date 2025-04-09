2f32973598ec3874a72d2abae89f57e9ca7e17e5
eyobScript <eyobbmulugeta@gmail.com>
2025-04-09 14:55:08 +0300

    make contwxt provider

--- frontend/src/contexts/MovieContext.jsx
+++ frontend/src/contexts/MovieContext.jsx
@@ -1,3 +1,24 @@
 import { createContext, useContext, useEffect  } from "react"
 
 
+const MovieContext = createContext();
+
+export const useMovieContext = useContext(MovieContext);
+
+export const MovieProvider = ({ children }) => {
+
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        // check if exist item in local storage and set it to state
+        const storedFev = localStorage.getItem("favorites");
+        if(storedFev) setFavorites(JSON.parse(storedFev));
+    }, []);
+
+    useEffect(() => {
+        // set the favorites to local storage when favorites state change
+        localStorage.setItem(JSON.stringify(favorites));
+    },[favorites])
+}
+
+
