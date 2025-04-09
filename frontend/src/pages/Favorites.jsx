d810daca00c8d6291f47c78683ab833b0d114d7c
eyobScript <eyobbmulugeta@gmail.com>
2025-04-09 16:48:32 +0300

    favorites page

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import '../css/Favorites.css';
-function Favorites(){
+import React from "react";
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites.length > 0) {
     return (
-        <div className="favorites-empty">
-            <h2>No favorite movie yet.</h2>
-            <p>Click on the heart icon to add a movie to your favorites.</p>
+      <div className="favorites">
+        <h2>Your favorite movies</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No favorite movie yet.</h2>
+      <p>Click on the heart icon to add a movie to your favorites.</p>
+    </div>
+  );
 }
 
 export default Favorites;
